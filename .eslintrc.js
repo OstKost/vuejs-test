1dc782045b8af61ee93de2f8a64f38cf78cbf437
OstKost <ostkost@list.ru>
2018-05-16 13:37:13 +0300

    меню и роутинг

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,12 @@ module.exports = {
     browser: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: [
+    'standard'    
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',    
   ],
   // add your custom rules here
   rules: {
