47d52c374863f4a5975b15f077cbe7e378102f8b
OstKost <ostkost@list.ru>
2018-05-17 13:15:07 +0300

    доработка регистрации с перенаправлением

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -61,6 +61,11 @@ export default {
           password: this.password
         }
         this.$store.dispatch('registerUser', user)
+          .then(() => {
+            this.$router.push('/')
+          }).catch((err) => {
+            console.log(err)
+          })
       }
     }
   }
--- src/store/user.js
+++ src/store/user.js
@@ -16,20 +16,20 @@ export default {
     }
   },
   actions: {
-    registerUser({ commit }, { email, password }) {
+    async registerUser({ commit }, { email, password }) {
       commit('clearError')
       commit('setLoading', true)
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          commit('setUser', new User(user.uid))
-          commit('setLoading', false)
-        })
-        .catch(error => {
-          commit('setLoading', false)
-          commit('setErroe', error.message)
-        })
+      try {
+        const user = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+        commit('setUser', new User(user.uid))
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setErroe', error.message)
+        throw error
+      }
     }
   },
   getters: {
