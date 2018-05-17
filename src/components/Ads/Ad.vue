e2801fd9fca9c0e8c503347653a87883428556aa
OstKost <ostkost@list.ru>
2018-05-17 10:32:03 +0300

    ad

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,24 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>AD</h1>
+                <v-card>
+                  <v-card-media                    
+                    height="260px"
+                    src="https://vuetifyjs.com/static/doc-images/carousel/squirrel.jpg"
+                  >
+                      
+                  </v-card-media>
+
+                  <v-card-text>
+                      <h1 class="text--primary">Lorem, ipsum dolor.</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cumque!</p>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="warning" flat>Edit</v-btn>
+                    <v-btn color="success">Buy</v-btn>
+                  </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
